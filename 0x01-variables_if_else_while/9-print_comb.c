fc16bde23aeb51ba75163452989ec25ff87e06f4
zoz <zezozeyad7654@gmail.com>
2023-03-17 10:08:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+	int n;
 
-	for (i = 0; i < 100; i++)
+	for (n = 48; n < 58; n++)
 	{
-		for (j = 0; j < 100; j++)
+		putchar(n);
+		if (n != 57)
 		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
