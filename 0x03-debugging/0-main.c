420a802a8aff61ce01ebd2cad13b63bcfec0b8b4
zoz <zezozeyad7654@gmail.com>
2023-03-23 07:15:56 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/**
+/*
 * main - Test function for positive or negative
 * Return: 0
 */
-
 int main(void)
 
 {
@@ -13,6 +12,5 @@ int main(void)
 
         i = 0;
         positive_or_negative(i);
-
         return (0);
 }
