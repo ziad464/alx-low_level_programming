cc965b06e4e075f536ee00fbdf8a5b8af2f75a9f
zoz <zezozeyad7654@gmail.com>
2023-03-23 08:39:24 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,15 @@
 /**
  *_isupper - uppercase letters
  *@c: char to check
- *
  *Return: 0 or 1
  */
+
 int_isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
-	else
+	}
 		return (0);
 }
