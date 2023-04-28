283bb53da66e501396d2953121ae91fa35bca3f0
ziad464 <zezozeyad7654@gmail.com>
2023-04-28 16:58:18 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include"lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
