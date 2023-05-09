2364537a49470df745e19dd074dbb3195abd1588
ziad464 <zezozeyad7654@gmail.com>
2023-05-08 18:52:20 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - reads text from a file and prints it
+ * @filename: name of file to read
+ * @letters: number of bytes to read
+ *
+ * Return: number bytes read/printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
-	fd = open(filename, O_RDONLY);
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, 0_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
-	close(fd);
-	return (w);
+		bytes = read(fd, &buf[0], letters);
+		bytes = write(STDOUT_FILENO, &buf[0], bytes);
+		close(fd);
+		return (bytes);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,40 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ *_strlen - returns the length of a string
+ *@s: the string whose length to check
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * create_file - creates a file
+ * @filename:name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on succes 0 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | OTRUNC, S_TRUSR | S_TWUSR);
+	if (fd == -1)
 		return (-1);
-
-	close(fd);
-
-	return (1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	closed(fd);
+	return (bytes == len ? 1 : -1);
 }
